1c32b1ad8c4bf4f53d92dfde76fd216dae987699
= <rahul.bhati@industrybuying.com>
2020-08-22 11:43:31 +0530

    zuul api gateway integrated

---
+++ src/main/java/com/rsb/demo/ZuulApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.rsb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApiGatewayApplication.class, args);
+    }
+}
+
